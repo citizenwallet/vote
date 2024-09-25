8406d35b3ef56d94edc9b4dc16de2b85810e8e72
kev-techi <kevin@techi.be>
2024-09-25 15:35:42 +0200

    polling > polls

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-teal-700">
-                  Anonymous In-Person Polling
+                  Anonymous In-Person Polls
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl">
                   Create verifiable, on-chain polls for any topic. Use NFC tags
