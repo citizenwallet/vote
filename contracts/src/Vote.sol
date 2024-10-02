836d1ca7ccece438d566ee3980c55274034554d3
kev-techi <kevin@techi.be>
2024-10-02 18:42:30 +0200

    update todo

--- contracts/src/Vote.sol
+++ contracts/src/Vote.sol
@@ -101,7 +101,7 @@ contract Vote is Initializable, OwnableUpgradeable, UUPSUpgradeable, AccessContr
     }
 
     function vote(bytes32 pollId, address voter, uint256 optionIndex) public onlyOpenPoll(pollId) {
-        // TODO: send token instead of voter address
+        // TODO: see if it's possible to vote without revealing the voter address
         bytes32 token = generatePollToken(pollOwners[pollId], voter);
 
         address pollOwner = pollOwners[pollId];
